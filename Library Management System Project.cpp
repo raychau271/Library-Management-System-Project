a88068b0a7013d32fd71f5cc977bf2e37c124562
RayChau <ray23chau@gmail.com>
2020-11-05 18:44:23 +0800

    Main Menu

--- Library Management System Project.cpp
+++ Library Management System Project.cpp
@@ -2,20 +2,26 @@
 //Raymond
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-	//Raymond Test
+int main(){
+
+	int option;
+
+	do{
+		cout << "Main Menu" << endl;
+		cout << endl;
+		cout << "01. Book Issue" << endl;
+		cout << endl;
+		cout << "02. Book Deposit" << endl;
+		cout << endl;
+		cout << "03. Adminstrator Menu" << endl;
+		cout << endl;
+		cout << "04. Exit" << endl;
+		cout << endl;
+		cout << "Please Select Your Option (1-4) ";
+		cin >> option;
+		cout << endl;
+	} while (option < 1 || option > 4);
+	
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
